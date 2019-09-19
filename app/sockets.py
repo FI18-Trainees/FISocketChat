465c3a70a88e08ea66fdb22b0f97608d6c63be82
SFFan123 <jack.o.neill.fabi@t-online.de>
2019-09-19 10:29:08 +0200

    added link conversion

--- app/sockets.py
+++ app/sockets.py
@@ -1,6 +1,7 @@
 from app import app, socketio, emotehandler
 from flask_socketio import send, emit
 import re
+from validators import url as valUrl
 
 
 @socketio.on('chat_message')
@@ -13,6 +14,9 @@ def handle_message(message):
 
     if len(msg[(msg.find(':')+1):].strip()) > 0:
         msg = safe_tags_replace(msg)
+        prefix = msg[:msg.find(": ")+2]
+        possiblelink = str.strip(msg[msg.find(": ")+2:])
+        msg = prefix + link_replacer(possiblelink)
         msg = safe_emote_replace(msg)
         emit('chat_message', usr + msg, broadcast=True)
 
@@ -53,3 +57,15 @@ def replaceEmote(emote):
 
 def safe_emote_replace(text):
     return re.sub(r"[\"'\/]?[\/:\w]+[\"'\/]?", lambda x: replaceEmote(x.group()), text, 0, re.MULTILINE)
+
+
+def link_replacer(text):
+    return re.sub(r"[:/.?=_\w]+", lambda x: linkwrapping(x.group()), text, 0, re.MULTILINE)
+
+def linkwrapping(text):
+    res = valUrl(text)
+    # print(res)
+    if res:
+        return "<a target=\"_blank\" rel=\"noopener noreferrer\" href=\"" + text + "\">" + text + "</a>"
+    else:
+        return text
