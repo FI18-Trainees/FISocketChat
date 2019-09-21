e48cfcaaeccc491dab23eeedd66b5544f03a27ae
Yannick <zaanposni@users.noreply.github.com>
2019-09-21 13:30:39 +0200

    added basic auth for /index and /public, maybe rethink this ? fixes #5

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,32 @@
 from flask import render_template, send_from_directory, request
 from app import app, emotehandler
+from flask_httpauth import HTTPBasicAuth
+import requests
+
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(username, password):
+    print("test")
+    r = requests.get("https://auth.zaanposni.com/verify",
+                     auth=(username, password),
+                     headers={'Cache-Control': 'no-cache'})
+    if r.text == "OK":
+        return True
+    return False
 
 
 @app.route('/')
 @app.route('/index')
+@auth.login_required
 def index():
+    print("a")
     return render_template('index.html', ip=request.remote_addr, emotes=emotehandler.emotes)
 
 
 @app.route('/public/<path:path>')
+@auth.login_required
 def send_public(path):
     return send_from_directory('public', path)
 
