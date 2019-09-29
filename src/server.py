f95dec21916e19e0ba7323e81756d99996795d12
Yannick <zaanposni@users.noreply.github.com>
2019-09-29 15:34:51 +0200

    refactored python code

--- src/server.py
+++ src/server.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 from app import app, socketio
+from app.shell import *
 import sys
+SHL = Console("Start")
 
 
 def run():
     port = 5000
     if "-port" in [x.strip().lower() for x in sys.argv]:
-        portpos = sys.argv.index("-port") + 1
-        if portpos <= len(sys.argv) - 1:
-            port = int(sys.argv[portpos])
+        try:
+            port = int(sys.argv[sys.argv.index("-port") + 1])
+            SHL.output(f"{red}Using custom port: {port}{white}")
             print("using custom port: " + str(port))
-
+        except IndexError:
+            pass
+        except ValueError:
+            sys.exit(f'Invalid port "{sys.argv[sys.argv.index("-port") + 1]}"')
+    SHL.output("Starting up.")
     socketio.run(app, host='0.0.0.0', port=port)
 
 
