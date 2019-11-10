bf052d5a6d11d4c68236cbff5113faf3b5b3880a
Yannick <zaanposni@users.noreply.github.com>
2019-11-10 10:06:39 +0100

    using oop in /list now

--- src/app/commands/list.py
+++ src/app/commands/list.py
@@ -2,6 +2,7 @@ from flask_socketio import emit
 
 from ..shell import *
 from .. import user_manager, logindisabled
+from ..obj import SystemMessenger, User, Command
 
 SHL = Console("Command List")
 
@@ -10,10 +11,10 @@ settings = {
 }
 
 
-def main(system, author, message, params):
+def main(system: SystemMessenger, author: User, message: Command, params: list):
     msg = f"There are {user_manager.get_count()} user(s) connected.<br />"
     if not logindisabled:
         for sid, cfg in user_manager.configs.items():
             msg += f"{cfg['username']}: " \
                    f"{cfg['userconfig']['display_name']} <br />"
-    system.system_emit(msg)
+    system.send(msg)
