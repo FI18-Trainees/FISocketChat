2fdf51483143159391ede39e8771d54cc19890bf
Yannick <zaanposni@users.noreply.github.com>
2019-11-10 10:06:27 +0100

    minor changes

--- src/app/obj/__init__.py
+++ src/app/obj/__init__.py
@@ -3,4 +3,4 @@ from .user import User, default_user
 from .command import Command
 from .user_manager import UserManager
 from .system_message import SystemMessage
-from .system import System
+from .system import system, SystemMessenger
