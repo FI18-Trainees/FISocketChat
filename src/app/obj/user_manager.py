cdd5745a39828b0b8d09906f7b82248380c834cb
zaanposni <zaanposni@users.noreply.github.com>
2019-11-11 07:49:27 +0100

    using User object in usermanager now

--- src/app/obj/user_manager.py
+++ src/app/obj/user_manager.py
@@ -2,26 +2,19 @@ from . import User
 
 
 class UserManager:
-    def __init__(self, init_value=0):
-        self.count = init_value
+    def __init__(self):
         self.configs = {}
-        self.users = {}
 
-    def add(self, sid: str, user: User, username=None, userconfig=None):
-        self.count += 1
+    def add(self, sid: str, user: User):
         self.configs[sid] = {  # TODO: use user object
-            "username": username,
-            "userconfig": userconfig
+            "user": user
         }
-        self.users[sid] = user
 
     def rem(self, sid):
-        self.count -= 1
         self.configs.pop(sid, None)
-        if self.count < 0: self.count = 0
 
     def get_count(self) -> int:
-        return self.count
+        return len(self.configs)
 
     def __str__(self):
-        return f"User count: {self.count}"
+        return f"User count: {self.get_count()}"
