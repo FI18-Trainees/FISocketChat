72629f29612f0bc305503fefdcf5fb12ce2e071e
SFFan123 <fof2fe@bosch.com>
2019-11-13 10:20:59 +0100

    autism strikes again

--- src/app/commands/gitversion.py
+++ src/app/commands/gitversion.py
@@ -1,9 +1,8 @@
-from app import user_manager, login_disabled
 from app.obj import SystemMessenger, User, Command
 from utils.shell import Console, white
 import subprocess
 
-SHL = Console("Command List")
+SHL = Console("Command gitversion")
 
 settings = {
     'invoke': 'gitversion',
@@ -13,10 +12,12 @@ settings = {
 git_version_short_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
 git_version_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
 git_remote_url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).decode('ascii').strip().replace(".git", "")
+
 SHL.output(f"{white}Found local version: {git_version_short_hash}", "gitversion")
+
+__generated_link = git_remote_url + "/commit/" + git_version_hash
+__msg = f"Current Version is <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"{__generated_link}\">{git_version_short_hash}</a>.<br />"
 
 
 def main(system: SystemMessenger, author: User, cmd: Command, params: list):
-    link = git_remote_url + "/commit/" + git_version_hash
-    msg = f"Current Version is <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"{link}\">{git_version_short_hash}</a>.<br />"
-    system.send(msg)
+    system.send(__msg)
