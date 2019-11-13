d75f0334a275a241e5cad3a69456382809186918
SFFan123 <fof2fe@bosch.com>
2019-11-13 10:44:04 +0100

    added try except

--- src/app/commands/gitversion.py
+++ src/app/commands/gitversion.py
@@ -1,5 +1,5 @@
 from app.obj import SystemMessenger, User, Command
-from utils.shell import Console, white
+from utils.shell import Console, white, red
 import subprocess
 
 SHL = Console("Command gitversion")
@@ -9,15 +9,25 @@ settings = {
 }
 
 # get Version
-git_version_short_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
-git_version_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
-git_remote_url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).decode('ascii').strip().replace(".git", "")
+try:
+    git_version_short_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+    git_version_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+    git_remote_url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).decode('ascii').strip().replace(".git", "")
 
-SHL.output(f"{white}Found local version: {git_version_short_hash}", "gitversion")
+    SHL.output(f"{white}Found local version: {git_version_short_hash}", "gitversion")
 
-__generated_link = git_remote_url + "/commit/" + git_version_hash
-__msg = f"Current Version is <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"{__generated_link}\">{git_version_short_hash}</a>.<br />"
+    __generated_link = git_remote_url + "/commit/" + git_version_hash
+    __msg = f"Current Version is <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"{__generated_link}\">{git_version_short_hash}</a>.<br />"
+
+except subprocess.CalledProcessError:
+    SHL.output(f"{red}Error getting git version", "gitversion")
+except UnicodeDecodeError:
+    SHL.output(f"{red}Error parsing git version", "gitversion")
 
 
 def main(system: SystemMessenger, author: User, cmd: Command, params: list):
-    system.send(__msg)
+    if __msg is not None:
+        system.send(__msg)
+    else:
+        system.send("Error fetching version.")
+
