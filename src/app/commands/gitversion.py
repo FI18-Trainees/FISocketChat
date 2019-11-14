1268f5fdd749172c74a1d431449130e65bda5670
SFFan123 <fof2fe@bosch.com>
2019-11-14 14:22:38 +0100

    refactor the method

--- src/app/commands/gitversion.py
+++ src/app/commands/gitversion.py
@@ -6,6 +6,7 @@ SHL = Console("Command gitversion")
 
 settings = {
     'invoke': 'gitversion',
+    'system_display_name': 'System - gitversion'
 }
 
 # get Version
@@ -17,7 +18,7 @@ try:
     SHL.output(f"{white}Found local version: {git_version_short_hash}", "gitversion")
 
     __generated_link = git_remote_url + "/commit/" + git_version_hash
-    __msg = f"Current Version is <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"{__generated_link}\">{git_version_short_hash}</a>.<br />"
+    __msg = f"Current running Version  is <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"{__generated_link}\">{git_version_short_hash}</a>.<br />"
 
 except subprocess.CalledProcessError:
     SHL.output(f"{red}Error getting git version", "gitversion")
@@ -26,13 +27,13 @@ except UnicodeDecodeError:
 
 
 def main(system: SystemMessenger, author: User, cmd: Command, params: list):
-    if len(params) > 0:
-        if params[0] == "help":
-            system.send("[gitversion help]: Displays the hash of the commit the chat is currently running of. "
-                        "It also links to the commit on the Repo site where you can view the code.")
-    else:
+    if not len(params):
         if __msg is not None:
             system.send(__msg)
         else:
             system.send("Error fetching version.")
+
+    if params[0] == "help":
+        system.send("[gitversion help]: Displays the hash of the commit the chat is currently running of. "
+                    "It also links to the commit on the Repo site where you can view the code.")
 
