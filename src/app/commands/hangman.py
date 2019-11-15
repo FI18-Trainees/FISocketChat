21ce63db174b0a874ccd5237844cf4332b1db83b
ArPiiX <simon-mue@gmx.de>
2019-11-15 08:39:11 +0100

    slight adjustments

--- src/app/commands/hangman.py
+++ src/app/commands/hangman.py
@@ -20,21 +20,25 @@ def main(system: SystemMessenger, author: User, cmd: Command, params: list):
     # game command should be                            /hangman start *word*
     # then it should be                                 /hangman guess *char*
     # to check game state when connection lost try     /hangman state
+
     if not len(params):
         system.send("'/hangman start *word*' starts a game of hangman with your own word.")
         return
+
     if params[0].lower() == "help":
         system.send("Usable commands:<br/>'/hangman start *word*'<br/>'/hangman guess *char*'<br/>"
                     "'/hangman solve *word*'<br/>'/hangman state'<br/>/hangman info")
         return
+
     if params[0].lower() == "info":
         system.send("In hangman you get to guess a word being set by the starter of the game. You will see how many"
                     "letters are in the word and then have to either guess each character with 7 possible mistakes"
                     "or you can try to guess the word at once making only one try possible.")
         return
-    if params[0].lower() == "start" and params[1].lower != "":
+
+    if params[0].lower() == "start" and params[1].lower() != "":
         if not hangman_game.get_state():
-            if not params[1].lower == " ":
+            if not params[1].lower() == " ":
                 hangman_game.reset_game()
                 hangman_game.start(params[1])
                 SHL.output(f"{author} started a game with: {params[1]}", "HangmanGame")  # log
@@ -43,6 +47,7 @@ def main(system: SystemMessenger, author: User, cmd: Command, params: list):
                 return
             system.send("No spaces!")
         system.send(f"Game already running!<br/>{hangman_game.get_word()}")
+
     if params[0].lower() == "guess":
         if hangman_game.get_state():
             if len(params[1]) != 1:
@@ -53,17 +58,20 @@ def main(system: SystemMessenger, author: User, cmd: Command, params: list):
             system.broadcast(hangman_game.check_char(params[1]))
             return
         system.send(no_game())
+
     if params[0].lower() == "solve":
         if hangman_game.get_state():
             SHL.output(f"{author} tried to solve {hangman_game.word_clear} with {params[1]}", "HangmanGame")  # log
             system.broadcast(hangman_game.check_word(params[1]))
             return
         system.send(no_game())
+
     if params[0].lower() == "state":
         if hangman_game.get_state():
             system.send(hangman_game.get_word())
             SHL.output(f"{author} fetched the state of the game.", "HangmanGame")  # log
             return
         system.send(no_game())
+
     system.send("Usable commands:<br/>'/hangman start *word*'<br/>'/hangman guess *char*'<br/>"
                 "'/hangman solve *word*'<br/>'state'")
