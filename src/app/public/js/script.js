5b0e79bc6f7aad201bbbda92b65dbd9bd1839f27
monkmitrad <bjoern@familie-biester.de>
2019-11-16 16:20:30 +0100

    merged overflow into script.js fixed some issues in html

--- src/app/public/js/script.js
+++ src/app/public/js/script.js
@@ -445,3 +445,19 @@ function imgloaded() {
         }
     }
 }
+
+// Determines if the passed element is overflowing its bounds, either vertically or horizontally.
+// Will temporarily modify the "overflow" style to detect this if necessary.
+function checkOverflow(el) {
+    var curOverflow = el.style.overflow;
+
+    if (!curOverflow || curOverflow === "visible")
+        el.style.overflow = "hidden";
+
+    var isOverflowing = el.clientWidth < el.scrollWidth
+        || el.clientHeight < el.scrollHeight;
+
+    el.style.overflow = curOverflow;
+
+    return isOverflowing;
+}
