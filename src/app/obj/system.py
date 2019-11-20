94d106c305b8257bfbc36eefd506488c1300e005
zaanposni <zaanposni@users.noreply.github.com>
2019-11-20 22:08:58 +0100

    making embeds sendable with systemmessenger

--- src/app/obj/system.py
+++ src/app/obj/system.py
@@ -3,24 +3,27 @@ from typing import Union
 from flask_socketio import emit
 
 from .system_message import SystemMessage
+from .embed import Embed
 
 
 class SystemMessenger:
     def __init__(self, display_name: str = None):
         self.display_name = display_name
 
-    def send(self, message: Union[SystemMessage, str]):
+    def send(self, message: Union[SystemMessage, str, Embed]):
         if isinstance(message, str):
             message = SystemMessage(msg_body=message)
         if self.display_name:
             message.change_display_name(self.display_name)
+
         emit('chat_message', message.to_json())
 
-    def broadcast(self, message: Union[SystemMessage, str]):
+    def broadcast(self, message: Union[SystemMessage, str, Embed]):
         if isinstance(message, str):
             message = SystemMessage(msg_body=message)
         if self.display_name:
             message.change_display_name(self.display_name)
+
         emit('chat_message', message.to_json(), broadcast=True)
 
     def change_display_name(self, new: str):
