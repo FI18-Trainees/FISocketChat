8d507691fbcd34b29b698b6fd70a9435cdd43694
SFFan123 <38184195+SFFan123@users.noreply.github.com>
2019-11-22 20:27:11 +0100

    +1

--- src/app/public/js/script.js
+++ src/app/public/js/script.js
@@ -495,31 +495,22 @@ function getMessageHistory() {
 }
 
 $('#fileinput').on('change', function () {
-  var file = this.files[0];
-
+  let file = this.files[0];
   if (file.size > 1024*1024*3) {
     alert('max upload size is 3M');
   }
-
   $.ajax({
-    // Your server script to process the upload
     url: '/api/upload',
     type: 'POST',
 
-    // Form data
     data: new FormData($('#form')[0]),
-
-    // Tell jQuery not to process data or worry about content-type
-    // You *must* include these options!
     cache: false,
     contentType: false,
     processData: false,
 
-    // Custom XMLHttpRequest
     xhr: function () {
       var myXhr = $.ajaxSettings.xhr();
       if (myXhr.upload) {
-        // For handling the progress of the upload
         myXhr.upload.addEventListener('progress', function (e) {
           if (e.lengthComputable) {
             $('progress').attr({
