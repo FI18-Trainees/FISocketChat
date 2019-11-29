39beac2b1037fa7899a59255eee6f612bc377fdb
zaanposni <zaanposni@users.noreply.github.com>
2019-11-29 09:57:51 +0100

    embed for /list

--- src/app/commands/list.py
+++ src/app/commands/list.py
@@ -1,18 +1,20 @@
 from app import user_manager, login_disabled
-from app.obj import SystemMessenger, User, Command
+from app.obj import SystemMessenger, User, Command, Embed
 from utils import Console
 
 SHL = Console("Command List")
 
 settings = {
     'invoke': 'list',
+    'system_display_name': 'System - List',
 }
 
 
 def main(system: SystemMessenger, author: User, cmd: Command, params: list):
-    msg = f"There are {user_manager.get_count()} user(s) connected.<br />"
-    if not login_disabled:
-        for user in user_manager.configs.values():
-            msg += f"{user['user'].username}: " \
-                   f"{user['user'].display_name} <br />"
-    system.send(msg)
+    e = Embed(title="Userlist", thumbnail="http://simpleicon.com/wp-content/uploads/users.png", color="#00ff00")
+    if login_disabled:
+        e.set_text("not available in logindisabled mode.")
+    else:
+        e.set_text("<br />".join([f"{x['user'].username}: {x['user'].display_name}"
+                                  for x in user_manager.configs.values()]))
+    system.send(e)
