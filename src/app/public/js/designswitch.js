fdc49a222c2db1af74c12553365abbcfecd7d947
Monkmitrad <monkmitrad@gmail.com>
2019-12-02 12:54:34 +0100

    implemented change of css href

--- src/app/public/js/designswitch.js
+++ src/app/public/js/designswitch.js
@@ -1,3 +1,7 @@
-$('theme-toggle').change(function() {
-    alert('Theme switched');
+$('#theme-toggle').change(function() {
+    if($('#theme-toggle').is(':checked')) {
+        $('#maincss').attr('href', '/public/css/fioratheme.css');
+    } else {
+        $('#maincss').attr('href', '/public/css/general.css');
+    }
 });
