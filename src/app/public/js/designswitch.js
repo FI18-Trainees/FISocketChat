03e360fd4819f43c12abd0b7ce8891370cf408db
Monkmitrad <monkmitrad@gmail.com>
2019-12-02 14:14:15 +0100

    moved themespecific css to own files

--- src/app/public/js/designswitch.js
+++ src/app/public/js/designswitch.js
@@ -1,7 +1,7 @@
 $('#theme-toggle').change(function() {
     if($('#theme-toggle').is(':checked')) {
-        $('#maincss').attr('href', '/public/css/fioratheme.css');
+        $('#themecss').attr('href', '/public/css/fioratheme.css');
     } else {
-        $('#maincss').attr('href', '/public/css/general.css');
+        $('#themecss').attr('href', '/public/css/normietheme.css');
     }
 });
