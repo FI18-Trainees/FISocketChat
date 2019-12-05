eff7fb14f7695598489ceb1d78beeffa82e1ac85
SFFan123 <fof2fe@bosch.com>
2019-12-05 13:08:41 +0100

    file check algorithm changed to sha1

--- src/app/routes.py
+++ src/app/routes.py
@@ -2,7 +2,7 @@
 import json
 import os
 from datetime import datetime
-from hashlib import md5
+from hashlib import sha1
 
 from flask import render_template, send_from_directory, make_response, jsonify, request, url_for, flash
 from werkzeug.utils import secure_filename
@@ -133,7 +133,7 @@ def upload_file():
             filename = secure_filename(str(datetime.now()) + "-" + file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            filemd5 = __gen_md5(filename)
+            filemd5 = __gen_hash(filename)
             if filemd5 in uploaded_files:
                 os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 return uploaded_files.get(filemd5)['url']
@@ -149,9 +149,9 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
 
-def __gen_md5(fname):
-    hash_md5 = md5()
+def __gen_hash(fname):
+    __hash = sha1()
     with open(os.path.join(app.config['UPLOAD_FOLDER'], fname), "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+            __hash.update(chunk)
+    return __hash.hexdigest()
