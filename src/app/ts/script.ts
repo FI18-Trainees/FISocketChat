d3d3f2f97b10c7dca81b4218fd604658f007d7f2
monkmitrad <monkmitrad@gmail.com>
2019-12-11 21:44:07 +0100

    changed tsconfig, updated gitignore, bugfixed" script.ts updated routes, compiled script.ts

--- src/app/ts/script.ts
+++ src/app/ts/script.ts
@@ -56,7 +56,7 @@ $('document').ready(function () {
 
 
     messagefield.keydown(function (e) {
-        e = e || window.event;
+        //e = e || window.event;
         switch (e.keyCode) {
             case 9:
                 //tab key
@@ -88,7 +88,7 @@ $('document').ready(function () {
                 break;
             case 40:
                 // down arrow
-                if (messagefield.val().trim() === "" || messagefield.val() === message_history[history_pointer]) {
+                if ((<string>messagefield.val()).trim() === "" || messagefield.val() === message_history[history_pointer]) {
                     history_pointer += 1;
                     if (history_pointer > message_history.length - 1) {
                         history_pointer = message_history.length - 1;
@@ -108,7 +108,7 @@ $('document').ready(function () {
         cooldown = window.setTimeout(function () {
             cooldown = 0
         }, 400);
-        if (messagefield.val().trim() === "") {
+        if ((<string>messagefield.val()).trim() === "") {
             showError('Invalid message.');
             return false;
         }
@@ -118,7 +118,7 @@ $('document').ready(function () {
         let u = $('#user_name').val().toString();
 
         let event_name = "chat_message";
-        if (messagefield.val().startsWith("/")) {
+        if ((<string>messagefield.val()).startsWith("/")) {
             event_name = "chat_command";
         }
 
@@ -189,8 +189,8 @@ $('document').ready(function () {
         }
 
         //check if chat would overflow currentSize and refresh scrollbar
-        if (checkOverflow(document.querySelector('#messages'))) { 
-            $('.nano').nanoScroller();
+        if (checkOverflow(document.querySelector('#messages'))) {
+            //$('.nano').nanoScroller();
             if(autoscroll) {
                 let chatdiv = document.querySelector('#messages');
                 chatdiv.scrollTop = chatdiv.scrollHeight;
@@ -265,7 +265,7 @@ $('document').ready(function () {
     });
 
     $('#notification-mode').change(function () {
-        notificationmode = this.value;
+        notificationmode = (<string>(<JQuery<HTMLSelectElement>>$('#notification-mode')).val());
         document.cookie = `notificationmode=${notificationmode}; expires=Thu, 01 Jan 2023 00:00:00 UTC; path=/`;
     });
 });
@@ -388,7 +388,7 @@ function addEmbed(msg) {
             case 'video':
                 let embed_video: JQuery<HTMLVideoElement> = $('<video class="video-embed" controls preload="metadata"/>');
                 embed_video.attr('src', media['media_url']);
-                embed_video.addEventListener('loadedmetadata', updateScroll);
+                //embed_video.addEventListener('loadedmetadata', updateScroll);
                 embed_media_container.append(embed_video);
                 break;
             case 'img':
@@ -532,7 +532,7 @@ function tabComplete(CursorPos) {
     let matches = Array.from(messageSplit['matchAll'](/[\r\n\t ]/gm));
     let lastSplit = 0;
     if (matches.length > 0) {
-        lastSplit = matches[matches.length - 1].index + 1;
+        lastSplit = (<RegExpMatchArray>matches[matches.length - 1]).index + 1;
     }
     let toComplete = messageSplit.substring(lastSplit);
     if (toComplete.length < 1)
@@ -614,7 +614,7 @@ function messagesScroll(event) {
 
 function mobileAndTabletcheck() {
     let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor);
     if(check) {
         $('#sendbtn').css('display', 'block');
     } else {
@@ -624,7 +624,7 @@ function mobileAndTabletcheck() {
 
 function updateScroll() {
     if (checkOverflow(document.querySelector('#messages'))) { //check if chat would overflow currentSize and refresh scrollbar
-        $('.nano').nanoScroller();
+        //$('.nano').nanoScroller();
         if(autoscroll) {
             let chatdiv = document.querySelector('#messages');
             chatdiv.scrollTop = chatdiv.scrollHeight;
@@ -677,7 +677,7 @@ function handleMessageHistory(history) {
         }
     }
     if (checkOverflow(document.querySelector('#messages'))) {
-        $('.nano').nanoScroller();
+        //$('.nano').nanoScroller();
         if(autoscroll) {
             let chatdiv = document.querySelector('#messages');
             chatdiv.scrollTop = chatdiv.scrollHeight;
@@ -685,15 +685,15 @@ function handleMessageHistory(history) {
     }
 }
 
-$('#fileinput').on('change', function (e) {
-  let file = this.files[0];
+/*$('#fileinput').on('change', function (e) {
+  let file = $('#fileinput').files[0];
   if (file.size > 1024*1024*3) {
     alert('max upload size is 3M');
     e.preventDefault();
     return false;
   }
   uploadImage(file);
-});
+});*/
 
 function uploadImage(file){
     let fd = new FormData();
@@ -756,3 +756,74 @@ function handlePaste(e){
 }
 
 
+
+
+
+//More info here: https://developer.mozilla.org/de/docs/Web/API/notification
+
+var notifications = new Array();
+
+window.onload = function () {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (checkPermission()) {
+    //    // If it's okay let's create a notification
+    //    new Notification("Welcome back!");
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function (permission) {
+            // If the user accepts, let's create a notification
+            if (checkPermission()) {
+                displayNotifyMode();
+                new Notification("This is how a notification would appear!");
+            }
+        });
+    }
+    // At last, if the user has denied notifications, and you 
+    // want to be respectful there is no need to bother them any more.
+};
+
+//create new notification with the variable "text" as content
+function newNotification(text) {
+    var notification = new Notification(text);
+    notifications.push(notification);
+}
+
+//check if permission to show notification is granted
+function checkPermission() {
+    if (Notification.permission === "granted") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function disconnectNotification() {
+    if (checkPermission()) {
+        newNotification("You have been disconnected from the chat!");
+    }
+}
+
+function closeAllNotifications() {
+    if(notifications.length > 0){
+        notifications.forEach(closeNotification);
+    }
+}
+
+function closeNotification(item){
+    item.close();
+}
+
+function displayNotifyMode() {
+    if(checkPermission() == true){
+        $('#notify-mode').css('display', 'flex');
+    } else {
+        $('#notify-mode').css('display', 'none');
+    }
+}
