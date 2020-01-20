d35cbe43556c03f7932d249a4d56d14f55497e74
Monkmitrad <monkmitrad@gmail.com>
2020-01-20 16:02:49 +0100

    added logic for emotemenu, added emotes.json for testing

--- angular/src/app/input/emote-menu/emote-menu.component.ts
+++ angular/src/app/input/emote-menu/emote-menu.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import Emotes from '../../../assets/emotes.json';
 
 @Component({
   selector: 'app-emote-menu',
   templateUrl: './emote-menu.component.html',
   styleUrls: ['./emote-menu.component.scss']
 })
-export class EmoteMenuComponent implements OnInit {
+export class EmoteMenuComponent implements OnInit, AfterViewInit {
 
-  emotelist: string[] = [];
+  @ViewChild('emoteMenu', { static: false }) emoteMenu: ElementRef;
+
+  emoteList = Object.keys(Emotes);
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.updateEmoteMenu();
   }
 
+  updateEmoteMenu() {
+    this.emoteList.forEach(key => {
+      if (Emotes[key].menuDisplay) {
+        const emoteitem = document.createElement('a');
+        emoteitem.classList.add('cursor-pointer');
+        emoteitem.innerHTML = Emotes[key].menuDisplayCode;
+        this.emoteMenu.nativeElement.append(emoteitem);
+      }
+    });
+  }
 }
